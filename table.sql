f84a4fdb808a6fc376aa82883cee9fec5c813e62
Sarfaraj Nawaz Chowdhury <sarfarajeee525@gmail.com>
2025-05-24 11:01:19 +0600

    first commit

---
+++ table.sql
@@ -0,0 +1,24 @@
+-- Active: 1747721925179@@127.0.0.1@5432@assignment_2
+create table student (
+    id serial primary key,
+    name varchar(100) not null,
+    age int not null,
+    grade varchar(10) not null
+);
+
+INSERT INTO student (name, age, grade) VALUES
+('Alice Johnson', 14, '9th'),
+('Benjamin Smith', 15, '10th'),
+('Catherine Lee', 13, '8th'),
+('Daniel Kim', 16, '11th'),
+('Emily Davis', 14, '9th'),
+('Franklin Turner', 17, '12th'),
+('Grace Park', 13, '8th'),
+('Henry Brown', 15, '10th'),
+('Isabella Garcia', 16, '11th'),
+('Jack Wilson', 17, '12th');
+
+select * from student;
+
+select * from student
+where id=1;
